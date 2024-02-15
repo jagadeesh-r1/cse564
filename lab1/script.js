51f33eccd3c0e69d7c1b343500bd99db3bec8d95
jagadeesh-r1 <mail4jagadeeshreddy@gmail.com>
2024-02-14 21:15:02 -0500

    testing js

--- lab1/script.js
+++ lab1/script.js
@@ -1,9 +1,24 @@
 // load csv file
-fetch('https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/owid-covid-data.csv')
+fetch('https://raw.githubusercontent.com/jagadeesh-r1/cse564/master/lab1/youtube_stats.csv')
     .then(response => response.text())
-    .then(data => {;
-        console.log(data);
+    .then(data => {
+        // console.log(data);
+        // store csv headers into a variable
+        const headers = data.split('\n')[0].split(',');
+        console.log(headers);
+        // get all data in 'rank' column in a variable
+        // const rankColumn = data.split('\n').slice(1).map(row => parseInt(row.split(',')[2]));
+        // console.log(rankColumn);
+
+
     })
     .catch(error => console.error('Error:', error));
+
+    var selectElement = document.getElementById('dropdown');
+
+    selectElement.addEventListener('change', function() {
+        var selectedOption = this.value;
+        console.log(selectedOption); // This will log the value of the selected option
+    });
 
 // handle dom events
