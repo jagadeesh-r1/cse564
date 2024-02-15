c576529923f9e1f0cfc295e8086b471bf6fb1c0d
jagadeesh-r1 <mail4jagadeeshreddy@gmail.com>
2024-02-15 14:53:13 -0500

    final draft changes

--- lab1/script.js
+++ lab1/script.js
@@ -2,13 +2,6 @@
 fetch('https://raw.githubusercontent.com/jagadeesh-r1/cse564/master/lab1/youtube_stats.csv')
     .then(response => response.text())
     .then(data => {
-        // console.log(data);
-        // store csv headers into a variable
-        // const headers = data.split('\n')[0].split(',');
-        // console.log(headers);
-        // get all data in 'rank' column in a variable
-        // const rankColumn = data.split('\n').slice(1).map(row => parseInt(row.split(',')[2]));
-        // console.log(rankColumn);
         const rows = data.split('\n');
         const headers = rows[0].split(',');
         var categorical_columns = [
@@ -37,6 +30,7 @@ fetch('https://raw.githubusercontent.com/jagadeesh-r1/cse564/master/lab1/youtube
             "unemployment_rate",
             "urban_population"
         ]
+
         const dataObjects = rows.slice(1)
             .map(row => {
                 const values = row.split(',');
@@ -50,10 +44,11 @@ fetch('https://raw.githubusercontent.com/jagadeesh-r1/cse564/master/lab1/youtube
                 // Check if all properties are present and not empty
                 return headers.every(header => obj[header] !== undefined && obj[header] !== '');
             });
-        console.log(dataObjects);
+        // console.log(dataObjects);
 
         var selectElement = document.getElementById('dropdown');
         var scatterCheckbox = document.getElementById('scatter');
+        var flipButton = document.getElementById('flip');
         let width = 960;
         let height = 500;
 
@@ -199,17 +194,20 @@ fetch('https://raw.githubusercontent.com/jagadeesh-r1/cse564/master/lab1/youtube
                     .attr('x', innerWidth / 2)
                     .attr('y', innerHeight + 30)
                     .attr('text-anchor', 'middle')
+                    .attr('fill', 'white')
                     .text(selectedOption);
                 g.append('text')
                     .attr('x', -innerHeight / 2)
                     .attr('y', -30)
                     .attr('transform', 'rotate(-90)')
                     .attr('text-anchor', 'middle')
+                    .attr('fill', 'white')
                     .text('Frequency');
                 g.append('text')
                     .attr('x', innerWidth / 2)
                     .attr('y', -10)
                     .attr('text-anchor', 'middle')
+                    .attr('fill', 'white')
                     .text('Histogram of ' + selectedOption + (denomination ? ' in ' + denomination +'s' : ''));
             }
 
@@ -269,11 +267,13 @@ fetch('https://raw.githubusercontent.com/jagadeesh-r1/cse564/master/lab1/youtube
                     .attr('x', innerWidth / 2)
                     .attr('y', innerHeight + 30)
                     .attr('text-anchor', 'middle')
+                    .attr('fill', 'white')
                     .text('Frequency');
                 g.append('text')
                     .attr('x', -innerHeight / 2)
                     .attr('y', -30)
                     .attr('transform', 'rotate(-90)')
+                    .attr('fill', 'white')
                     .attr('text-anchor', 'middle')
                     .text(selectedOption);
 
@@ -281,12 +281,13 @@ fetch('https://raw.githubusercontent.com/jagadeesh-r1/cse564/master/lab1/youtube
                     .attr('x', innerWidth / 2)
                     .attr('y', -10)
                     .attr('text-anchor', 'middle')
+                    .attr('fill', 'white')
                     .text('Histogram of ' + selectedOption + (denomination ? ' in ' + denomination +'s' : ''));
             }
-            if (neumerical_columns.includes(selectedOption)) {
+            if (neumerical_columns.includes(selectedOption) && !scatterCheckbox.checked) {
                 createVerticalHistogram();
             }
-            else {
+            else if (categorical_columns.includes(selectedOption) && !scatterCheckbox.checked) {
                 createVerticalBarChart();
             }
 
@@ -357,6 +358,7 @@ fetch('https://raw.githubusercontent.com/jagadeesh-r1/cse564/master/lab1/youtube
                     .attr('height', ([key, value]) => innerHeight - y(value)); // End with the final height
                 g.append('text')
                     .attr('x', innerWidth / 2)
+                    .attr('fill', 'white')
                     .attr('y', innerHeight + 30)
                     .attr('text-anchor', 'middle')
                     .text(selectedOption);
@@ -365,11 +367,13 @@ fetch('https://raw.githubusercontent.com/jagadeesh-r1/cse564/master/lab1/youtube
                     .attr('y', -30)
                     .attr('transform', 'rotate(-90)')
                     .attr('text-anchor', 'middle')
+                    .attr('fill', 'white')
                     .text('Frequency');
                 g.append('text')
                     .attr('x', innerWidth / 2)
                     .attr('y', -10)
                     .attr('text-anchor', 'middle')
+                    .attr('fill', 'white')
                     .text('Bar chart of ' + selectedOption);
             }
 
@@ -440,17 +444,20 @@ fetch('https://raw.githubusercontent.com/jagadeesh-r1/cse564/master/lab1/youtube
                     .attr('x', innerWidth / 2)
                     .attr('y', innerHeight + 30)
                     .attr('text-anchor', 'middle')
+                    .attr('fill', 'white')
                     .text('Frequency');
                 g.append('text')
                     .attr('x', -innerHeight / 2)
                     .attr('y', -30)
                     .attr('transform', 'rotate(-90)')
+                    .attr('fill', 'white')
                     .attr('text-anchor', 'middle')
                     .text(selectedOption);
                 g.append('text')
                     .attr('x', innerWidth / 2)
                     .attr('y', -10)
                     .attr('text-anchor', 'middle')
+                    .attr('fill', 'white')
                     .text('Bar chart of ' + selectedOption);
             }
 
@@ -460,7 +467,7 @@ fetch('https://raw.githubusercontent.com/jagadeesh-r1/cse564/master/lab1/youtube
             else {
                 d3.select('#flip').on('click', function() {
                     d3.select('svg').remove();
-                    console.log(isVertical);
+                    // console.log(isVertical);
                     if (isVertical) {
                         if (neumerical_columns.includes(selectedOption)) {
                             isVertical = !isVertical;
@@ -486,13 +493,190 @@ fetch('https://raw.githubusercontent.com/jagadeesh-r1/cse564/master/lab1/youtube
             }
         });
 
+        scatterCheckbox.addEventListener('change', function() {
+            var radioContainer = document.getElementById('radio-container');
+            radioContainer.innerHTML = ''; // Clear the container
+            if (this.checked) {
+                // Create the X radio button
+                var xRadio = document.createElement('input');
+                xRadio.type = 'radio';
+                xRadio.id = 'xRadio';
+                xRadio.name = 'axis';
+                xRadio.value = 'x';
+                var xLabel = document.createElement('label');
+                xLabel.for = 'xRadio';
+                xLabel.textContent = 'X';
+        
+                // Create the Y radio button
+                var yRadio = document.createElement('input');
+                yRadio.type = 'radio';
+                yRadio.id = 'yRadio';
+                yRadio.name = 'axis';
+                yRadio.value = 'y';
+                var yLabel = document.createElement('label');
+                yLabel.for = 'yRadio';
+                yLabel.textContent = 'Y';
+        
+                // Append the radio buttons to the container
+                radioContainer.appendChild(xLabel);
+                radioContainer.appendChild(xRadio);
+                radioContainer.appendChild(yLabel);
+                radioContainer.appendChild(yRadio);
+
+                var xOption, yOption;
+
+                // Add event listeners to the radio buttons
+                xRadio.addEventListener('change', function() {
+                    if (this.checked) {
+                        xOption = selectElement.value;
+                        if (xOption && yOption) {
+                            console.log(xOption + ' xoption');
+                            console.log(yOption + ' yoption');
+                            drawScatterplot(xOption, yOption);
+                        }
+                    }
+                });
+
+                yRadio.addEventListener('change', function() {
+                    if (this.checked) {
+                        yOption = selectElement.value;
+                        if (xOption && yOption && scatterCheckbox.checked) {
+                            console.log(xOption + ' xoption');
+                            console.log(yOption + ' yoption');
+                            drawScatterplot(xOption, yOption);
+                        }
+                    }
+                });
+
+                // Add a 'change' event listener to the select element
+                selectElement.addEventListener('change', function() {
+                    if (xRadio.checked) {
+                        xOption = this.value;
+                    } else if (yRadio.checked) {
+                        yOption = this.value;
+                    }
+
+                    if (xOption && yOption && scatterCheckbox.checked) {
+                        console.log(xOption + ' xoption');
+                        console.log(yOption + ' yoption');
+                        drawScatterplot(xOption, yOption);
+                    }
+                });
+
+                flipButton.addEventListener('click', function() {
+                    // Swap xOption and yOption
+                    var temp = xOption;
+                    xOption = yOption;
+                    yOption = temp;
+                
+                    // Redraw the scatterplot with the new options
+                    if (xOption && yOption && scatterCheckbox.checked) {
+                        console.log(xOption + ' xoption');
+                        console.log(yOption + ' yoption');
+                        drawScatterplot(xOption, yOption);
+                    }
+                });
+
+                // Function to draw a scatterplot
+                function drawScatterplot(xOption, yOption) {
+                    // Assuming dataObjects is your data array
+                    const xData = dataObjects.map(row => isNaN(row[xOption]) ? row[xOption] : parseInt(row[xOption]));
+                    const yData = dataObjects.map(row => isNaN(row[yOption]) ? row[yOption] : parseInt(row[yOption]));
+                    console.log(xData);
+                    console.log(yData);
+
+                    // Your existing code to create an SVG, define scales, and draw circles
+                    d3.select('svg').remove();
+                    var svg = d3.select('#chart').append('svg')
+                        .attr('width', width)
+                        .attr('height', height);
+                    var margin = { top: 30, right: 30, bottom: 90, left: 60 };
+                    var innerWidth = width - margin.left - margin.right;
+                    var innerHeight = height - margin.top - margin.bottom;
+                    var x;
+                    if (isNaN(xData[0])) { // If xData is categorical
+                        x = d3.scaleBand()
+                            .domain(xData)
+                            .range([0, innerWidth])
+                            .padding(0.1);
+                    } else { // If xData is numerical
+                        x = d3.scaleLinear()
+                            .domain([d3.min(xData), d3.max(xData)])
+                            .range([0, innerWidth]);
+                        }
+                    var y;
+                    if (isNaN(yData[0])) { // If yData is categorical
+                        y = d3.scaleBand()
+                            .domain(yData)
+                            .range([innerHeight, 0])
+                            .padding(0.1);
+                    } else { // If yData is numerical
+                        y = d3.scaleLinear()
+                            .domain([d3.min(yData), d3.max(yData)])
+                            .range([innerHeight, 0]);
+                    }
+                    // var x = d3.scaleLinear()
+                    //     .domain([d3.min(xData), d3.max(xData)])
+                    //     .range([0, innerWidth]);
+                    // var y = d3.scaleLinear()
+                    //     .domain([d3.min(yData), d3.max(yData)])
+                    //     .range([innerHeight, 0]);
+                    
+                    var g = svg.append('g')
+                        .attr('transform', `translate(${margin.left},${margin.top})`);
+                    g.append('g')
+                        .attr('transform', `translate(0,${innerHeight})`)
+                        .call(d3.axisBottom(x))
+                        .selectAll("text")  
+                        .style("text-anchor", "end")
+                        .attr("dx", "-.8em")
+                        .attr("dy", ".15em")
+                        .attr('fill', 'white')
+                        .attr("transform", "rotate(-65)");
+                    g.append('g')
+                        .call(d3.axisLeft(y));
+
+                    var r = d3.scaleSqrt()
+                        .domain([0, d3.max(dataObjects, d => d.value)])
+                        .range([2, 20]);
+                    g.selectAll('circle').data(dataObjects) 
+                        .enter().append('circle')
+                        .attr('cx', d => x(isNaN(d[xOption]) ? d[xOption] : parseInt(d[xOption])))
+                        .attr('cy', d => y(isNaN(d[yOption]) ? d[yOption] : parseInt(d[yOption])))
+                        .attr('r', 7)
+                        .attr('fill', d => d3.interpolateRainbow(Math.random()))
+                        .append('title') // Append a title to each circle
+                        .text(d => `x: ${d[xOption]}, y: ${d[yOption]}`);
+                    g.append('text')
+                        .attr('x', innerWidth / 2)
+                        .attr('y', innerHeight + 30)
+                        .attr('text-anchor', 'middle')
+                        .attr('fill', 'white')
+                        .text(xOption);
+                    g.append('text')
+                        .attr('x', -innerHeight / 2)
+                        .attr('y', -30)
+                        .attr('transform', 'rotate(-90)')
+                        .attr('fill', 'white')
+                        .attr('text-anchor', 'middle')
+                        .text(yOption);
+                    g.append('text')
+                        .attr('x', innerWidth / 2)
+                        .attr('y', -10)
+                        .attr('text-anchor', 'middle')
+                        .attr('fill', 'white')
+                        .text('Scatterplot of ' + xOption + ' and ' + yOption);
+
+                }
+            }
+            else {
+                radioContainer.innerHTML = ''; // Clear the container
+                d3.select('svg').remove();
+                var svg = d3.select('#chart').append('svg')
+                    .attr('width', width)
+                    .attr('height', height);
+            }
+        });
+
     })
     .catch(error => console.error('Error:', error));
-
-
-
-
-
-
-
-// handle dom events
